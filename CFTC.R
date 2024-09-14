a65d4a99e24d4a2b60971652a92510510c9e07e8
kwill1992 <kwill1992@hotmail.com>
2024-09-14 16:37:14 -0400

    udpdate no big files

--- CFTC.R
+++ CFTC.R
@@ -2,12 +2,29 @@
 library(tidyverse)
 library(readxl)
 library(stringr)
-library(rvest)
+library(rvest) #for webscraping; it is also in tidyverse
+
+# webscraping help
+# https://r4ds.hadley.nz/webscraping
+# https://preettheman.medium.com/best-web-scraping-packages-for-r-2787979b747
+# https://scrapingant.com/blog/r-web-scraping
+# https://steviep42.github.io/webscraping/book/
+# https://statsandr.com/blog/web-scraping-in-r/
+# https://www.zenrows.com/blog/web-scraping-r
+# https://www.bigbookofr.com/chapters/getting%20cleaning%20and%20wrangling%20data
+# https://oxylabs.io/blog/web-scraping-r
+# https://brightdata.com/blog/how-tos/web-scraping-with-r
+
+# t
+
+
+
 
 # get financials
 ctcf_financials <- read_xls("FinFutYY.xls")
 ctcf_financials
 
+# test
 
 
 
@@ -21,3 +38,35 @@ ctcf_commodities <- ctcf_commodities %>% filter(str_detect(Market_and_Exchange_N
 # webscraping for 10Y
 webpage <- read_html("https://www.cftc.gov/sites/default/files/files/dea/cotarchives/2023/futures/deacbtlf102423.htm")
 webpage
+webpage <- read_html("https://www.cftc.gov/dea/futures/other_lf.htm")
+webpage
+# txt file
+txt.file <- webpage %>% html_element("other_lf.txt")
+text_stuff <- webpage %>%  html_text2()
+text_stuff
+text_stuff2 <- webpage %>%  html_text()
+text_stuff2
+
+
+
+# Use .csv files
+fullFile_FUT86_06 <- read_xls("FUT86_06.xls")
+fullFile_FUT86_06
+palladium_FUT86_06 <- fullFile_FUT86_06 %>% filter(str_detect( Market_and_Exchange_Names, "PALL"))
+
+fullFile_FUT07_14 <- read_xls("FUT07_14.xls")
+fullFile_FUT07_14
+palladium_FUT07_14 <- fullFile_FUT07_14 %>% filter(str_detect( Market_and_Exchange_Names, "PALL"))
+
+fullFile_FUT15_16 <- read_xls("FUT15_16.xls")
+fullFile_FUT15_16
+palladium_FUT15_16 <- fullFile_FUT15_16 %>% filter(str_detect( Market_and_Exchange_Names, "PALL"))
+
+fullFile_FUT17 <- read_xls("FUT17.xls")
+fullFile_FUT17
+palladium_FUT17 <- fullFile_FUT17 %>% filter(str_detect( Market_and_Exchange_Names, "PALL"))
+
+library(ggplot2)
+gg <- ggplot(palladium_FUT15_16, aes(x=Report_Date_as_MM_DD_YYYY,y=Comm_Positions_Long_All)) +
+  geom_line()
+plot(gg)
